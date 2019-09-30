3099ac252147238704b4a0a6793b866d828ac1f6
Claes Östlund <claes@stenbergit.se>
2019-09-30 11:24:02 +0200

    Bug fix.

--- PluginBootstrapNavtabs_v1.php
+++ PluginBootstrapNavtabs_v1.php
@@ -1,6 +1,7 @@
 <?php
 class PluginBootstrapNavtabs_v1{
   public function widget_include($data){
+    wfPlugin::enable('wf/embed');
     $widget = wfDocument::createWidget('wf/embed', 'embed', array('type' => 'script', 'file' => '/plugin/bootstrap/navtabs_v1/js/PluginBootstrapNavtabs_v1.js'));
     wfDocument::renderElement(array($widget));
   }
